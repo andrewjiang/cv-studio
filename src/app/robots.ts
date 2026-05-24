059e29417354989c93f9334621d68a0efb77cf1d
Andrew Jiang <andrew.h.jiang@gmail.com>
2026-05-24 15:34:18 -0700

    Add SEO sitemap and robots routes (#41)

---
+++ src/app/robots.ts
@@ -0,0 +1,22 @@
+import type { MetadataRoute } from "next";
+import { getTinyCvAppUrl } from "@/app/_lib/site-metadata";
+
+export default function robots(): MetadataRoute.Robots {
+  const appUrl = getTinyCvAppUrl();
+
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+      disallow: [
+        "/account",
+        "/account/",
+        "/api/",
+        "/claim/",
+        "/internal/",
+        "/studio/",
+      ],
+    },
+    sitemap: new URL("/sitemap.xml", appUrl).toString(),
+  };
+}
